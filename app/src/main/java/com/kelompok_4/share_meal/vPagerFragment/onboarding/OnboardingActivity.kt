a28625a23d10ae7d6a415af7dbd1e5ffefcba737
MUHAMMAD ARIEF RAHMAN <arief21si@mahasiswa.pcr.ac.id>
2023-12-21 20:46:58 +0700

    feature | Disable back on onboarding

--- app/src/main/java/com/kelompok_4/share_meal/vPagerFragment/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/kelompok_4/share_meal/vPagerFragment/onboarding/OnboardingActivity.kt
@@ -1,5 +1,6 @@
 package com.kelompok_4.share_meal.vPagerFragment.onboarding
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kelompok_4.share_meal.R
@@ -8,5 +9,11 @@ class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onBackPressed() {
+        // Disable back button
+
     }
 }
